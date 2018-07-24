db26044abd93289d463f99c6ea7f946c972f0a91
K2108 <kritikajohari21@gmail.com>
2018-07-24 21:02:31 +0800

    Update model.py

--- Practice/Sequential_Model/model.py
+++ Practice/Sequential_Model/model.py
@@ -7,3 +7,6 @@ model.add(Dense(units=10, activation='softmax'))
 model.compile(loss='categorical_crossentropy',
               optimizer='sgd',
               metrics=['accuracy'])
+model.fit(x_train, y_train, epochs=5, batch_size=32)
+loss_and_metrics = model.evaluate(x_test, y_test, batch_size=128)
+classes = model.predict(x_test, batch_size=128)
