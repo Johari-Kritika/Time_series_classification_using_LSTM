37948e13c59085a064d38a9463ccc3c0ed111363
K2108 <kritikajohari21@gmail.com>
2018-07-24 23:19:16 +0800

    Update storeSegments.py

--- Data_Extraction/storeSegments.py
+++ Data_Extraction/storeSegments.py
@@ -15,15 +15,15 @@ from numpy import genfromtxt
 	def fill_missing_values(some_list,buffer_size):
 		valid_values = []
 		for i in range(0,len(some_list)):
-			if some_list[i] != -1:
+			if some_list[i] != -1:	#Pupil size can't be -1
 				valid_pupil_value = some_list[i]
-				if len(valid_values) == buffer_size:
+				if len(valid_values) == buffer_size:	#Taking only the recent buffer_size values
 					valid_values.pop(0)
 				valid_values.append(valid_pupil_value)
 			else:
 				if len(valid_values) == buffer_size:
 					mean_value = sum(valid_values)/buffer_size
-					some_list[i] = mean_value
+					some_list[i] = mean_value	#Replace -1 with the mean of previous buffer_size values
 
 	def rectify_response_status(some_list):
 		for i in range(0,len(some_list)):
@@ -48,11 +48,11 @@ from numpy import genfromtxt
 
 	def extract_segments(some_list,some_list_1,userid):
 		list_diff_load = some_list - some_list.shift(-1)
-		k = list_diff_load.nonzero()
-		k1 = k[0]
-		num_of_tasks = (len(k1)-1)/2
-		max_ = max_timestep_taken(k1)
-		extracted_data_middle = np.zeros([num_of_tasks, max_+1])
+		k = list_diff_load.nonzero()	#Checking for the rises and falls in load plot
+		k1 = k[0]	#convert into a list
+		num_of_tasks = (len(k1)-1)/2	#Number of tasks attempted by a user
+		max_ = max_timestep_taken(k1)	#Maximum timestep taken for any task by a user
+		extracted_data_middle = np.zeros([num_of_tasks, max_+1])	#Storing only the task-related timesteps
 		for l in range(0,len(k1)-1):
 			if l%2 ==0:
 				num_of_timestep = k1[l+1] - k1[l]-1
