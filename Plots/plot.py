be8ce454fa1ae7ed6c7a7f4e961d0b497cf0d580
K2108 <kritikajohari21@gmail.com>
2018-07-27 19:25:17 +0530

    Update plot.py

--- Plots/plot.py
+++ Plots/plot.py
@@ -1,1 +1,16 @@
-
+# Input data file consist of 5 relevant columns
+# Column 2 : PupilLeft
+# Column 3 : PupilRight
+# Column 4 : LoadLevel
+# Column 5 : Time_in_sec
+# Column 6 : Diff_in_time
+import pandas as pd
+folder_name = ""
+for i in range(0,100):
+  filename = folder_name + str(i) +".csv"   #get the name of the .csv file
+  try:
+    df = pd.read_csv(filename, header=None)
+    print(i)
+  except:
+    print("Kritika")
+  print("It still works")
