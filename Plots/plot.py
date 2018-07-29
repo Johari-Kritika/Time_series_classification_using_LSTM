352dd86df12679d5e1a0725c29ba0731da272d57
K2108 <kritikajohari21@gmail.com>
2018-07-29 20:09:12 +0530

    Update plot.py

--- Plots/plot.py
+++ Plots/plot.py
@@ -5,10 +5,11 @@
 # Column 5 : Time_in_sec
 # Column 6 : Diff_in_time
 import pandas as pd
-folder_name = "E:\Kyanite\Data Analysis\Testing files\try"
+folder_name = "E:\Kyanite\Data Analysis\Testing files"
 print(folder_name)
 for i in range(1,5):
-  filename = folder_name + str(i) +".csv"   #get the name of the .csv file
+  filename = folder_name + "\try"+str(i) +".csv"   #get the name of the .csv file
+  print(filename)
   try:
     df = pd.read_csv(filename, header=None)
   except:
