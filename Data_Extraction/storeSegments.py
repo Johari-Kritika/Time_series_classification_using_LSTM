8f67b6e6cbdf5a129bf5c04fa0ccbf96fa318715
K2108 <kritikajohari21@gmail.com>
2018-07-30 16:11:23 +0800

    Add files via upload

--- Data_Extraction/storeSegments.py
+++ Data_Extraction/storeSegments.py
@@ -4,39 +4,41 @@ import pandas as pd
 import numpy as np
 from sklearn import preprocessing
 from numpy import genfromtxt
-	def read_pupil_load(dataframe):
-		df = pd.read_csv(dataframe)
-		countRow = df.shape[0]
-		count = 0
-		list_pupil_left = df['PupilLeft']
-		list_load_level = df['LoadLevel']
-		return list_pupil_left,list_load_level
+def read_pupil_load(dataframe):
+	df = dataframe
+	countRow = df.shape[0]
+	count = 0
+	list_pupil_left = df['PupilLeft'].tolist()
+	list_load_level = df['LoadLevel'].tolist()
+	return list_pupil_left,list_load_level
 
-	def fill_missing_values(some_list,buffer_size):
-		valid_values = []
-		for i in range(0,len(some_list)):
-			if some_list[i] != -1:	#Pupil size can't be -1
-				valid_pupil_value = some_list[i]
-				if len(valid_values) == buffer_size:	#Taking only the recent buffer_size values
-					valid_values.pop(0)
-				valid_values.append(valid_pupil_value)
-			else:
-				if len(valid_values) == buffer_size:
-					mean_value = sum(valid_values)/buffer_size
-					some_list[i] = mean_value	#Replace -1 with the mean of previous buffer_size values
+def fill_missing_values(some_list,buffer_size):
+	valid_values = []
+	for i in range(0,len(some_list)):
+		if some_list[i] != -1:	#Pupil size can't be -1
+			valid_pupil_value = some_list[i]
+			if len(valid_values) == buffer_size:	#Taking only the recent buffer_size values
+				valid_values.pop(0)
+			valid_values.append(valid_pupil_value)
+		else:
+			if len(valid_values) == buffer_size:
+				mean_value = sum(valid_values)/buffer_size
+				some_list[i] = mean_value	#Replace -1 with the mean of previous buffer_size values
+		print(i)
+	return some_list
 
-	def rectify_response_status(some_list):
-		for i in range(0,len(some_list)):
-			if some_list[i] != 0:
-				count = count + 1
-				value = some_list[i]
-				if count == 1:
-					first_value = value
-				if value != first_value:
-					some_list[i]=first_value
-			else:
-				count = 0
-		return some_list
+def rectify_response_status(some_list):
+	for i in range(0,len(some_list)):
+		if some_list[i] != 0:
+			count = count + 1
+			value = some_list[i]
+			if count == 1:
+				first_value = value
+			if value != first_value:
+				some_list[i]=first_value
+		else:
+			count = 0
+	return some_list
 
 	def max_timestep_taken(some_list):
 		max_ = 0
